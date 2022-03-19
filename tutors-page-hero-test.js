be36e6a26b8efb8323992bb99ad3a414309ec9ba
Yes Lee <joshuayes@gmail.com>
2022-03-20 02:51:13 +0800

    wip

--- tutors-page-hero-test.js
+++ tutors-page-hero-test.js
@@ -10,39 +10,42 @@ const { isEqual, orderBy, concat } = require("lodash/fp");
 const RAILS_API_BASE_URL = "http://localhost:3000";
 
 const countries = [
-  // pass
-  // ["en", "en"],
-  // ["tw", "zh-TW"],
-
-  // fail
-  // ["es", "es"],
-  // ["fr", "fr"],
-  // ["hk", "zh-HK"],
+  ["en", "en"],
+  ["tw", "zh-TW"],
   ["jp", "ja"],
-  // ["kr", "ko"],
-  // ["uk", "en-GB"],
-  // ["au", "en-AU"],
-  // ["ca", "en-CA"],
-  // ["cn", "zh-CN"],
-  // ["pt", "pt"],
-  // ["th", "th"],
+  ["kr", "ko"],
+  ["au", "en-AU"],
+  ["pt", "pt"],
+  ["th", "th"],
+  ["cn", "zh-CN"],
+  ["hk", "zh-HK"],
+  ["fr", "fr"],
+  ["es", "es"],
+  ["uk", "en-GB"],
+  ["ca", "en-CA"],
 ];
 const languages = [
   null,
   "english",
   "chinese",
-  // "japanese",
+  "japanese",
 ];
 const tags = [null, "business", "children"];
 const tagIds = [null, "121-5", "121-2"];
 const cities = [
   [null, null],
+  ["au", "en_australia_sydney"],
+  ["ca", "en_canada_ottawa"],
   ["en", "new_york_city_ny"],
   ["es", "es_spain_zaragoza"],
   ["fr", "fr_france_ajaccio"],
-  ["tw", "taipei"],
+  ["hk", "sha_tin"],
   ["jp", "tokyo"],
   ["kr", "kr_korea_tongyeong"],
+  ["pt", "shilin"],
+  ["th", "neihu"],
+  ["tw", "taipei"],
+  ["uk", "en_united_kingdom_sheffield"],
 ];
 const offlines = [null, "classes-near-me"];
 
@@ -53,7 +56,7 @@ const offlines = [null, "classes-near-me"];
         const tag = tags[tagIndex];
         if (!language && tag) continue;
         for (const [cityCountry, city] of cities) {
-          if (cityCountry !== country) continue;
+          if (cityCountry && cityCountry !== country) continue;
           for (const offline of offlines) {
             if (city && offline) continue;
             try {
@@ -74,23 +77,21 @@ const offlines = [null, "classes-near-me"];
                 params: { city, offline },
               });
 
-              const expectedTitle = /<h2 class="wall-title at-text-shadow is-hidden-mobile" [^>]*>[^>]*<\/h2></
-                .exec(expectedHtml)[0]
-                .replace(/<h2 [^>]*>/, "")
-                .replace(/<\/h2></, "");
-              const expectedMobileTitle = /<h2 class="wall-title at-text-shadow is-hidden-tablet" [^>]*>(.*)<\/h2></
-                .exec(expectedHtml)[0]
-                .replace(/<h2 [^>]*>/, "")
-                .replace(/<\/h2></, "");
-              const expectedSubtitle = /<h3 class="wall-subtitle at-text-shadow" [^>]*>[^>]*<\/h3></
-                .exec(expectedHtml)[0]
-                .replace(/<h3 [^>]*>/, "")
-                .replace(/<\/h3></, "");
+              const expectedTitle = /<h2 class="wall-title at-text-shadow is-hidden-mobile".*?>(.*?)<\/h2>/.exec(
+                expectedHtml
+              )[1];
+              const expectedMobileTitle = /<h2 class="wall-title at-text-shadow is-hidden-tablet".*?>(.*?)<\/h2>/.exec(
+                expectedHtml
+              )[1];
+              const expectedSubtitle = /<h3 class="wall-subtitle at-text-shadow".*?>(.*?)<\/h3>/.exec(expectedHtml)[1];
 
               if (
                 isEqual(pageData.page_hero.title, expectedTitle) &&
                 isEqual(pageData.page_hero.mobile_title, expectedMobileTitle) &&
-                isEqual(pageData.page_hero.subtitle, expectedSubtitle)
+                isEqual(
+                  pageData.page_hero.subtitle.replace(/\d* student reviews|\d* avis/, "1234 student reviews"),
+                  expectedSubtitle.replace(/\d* student reviews|\d* avis/, "1234 student reviews")
+                )
               ) {
                 console.log(country, locale, language, tag, city, offline, chalk.green("pass"));
               } else {
